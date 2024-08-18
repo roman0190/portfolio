2019e40aa0de3311a546b70ffc19c04c8fbaaa84
HOWLADAR, ROMAN <131019051+roman0190@users.noreply.github.com>
2024-08-18 09:19:08 +0600

    a little change

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -7,10 +7,10 @@ const ProjectCard = ({imgUrl, title, description,gitUrl,previewUrl}) => {
       className='h-56 md:h-96 rounded-t-xl relative group'
       style={{ background: `url(${imgUrl})` , backgroundSize:'cover'}}>
         <div className='overlay items-center justify-center absolute h-full w-full bg-slate-400 top-0 left-0 rounded-t-xl hidden bg-opacity-0 group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 '>
-        <Link href={gitUrl} className='m-2 h-11 w-28 flex justify-center border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
+        <Link href={gitUrl} className='m-2 h-11 w-28 flex justify-center border-2 relative rounded-full border-[#0987e1] hover:border-white group/link'>
           <p className=' text-[#0987e1] cursor-pointer group-hover/link:text-white m-2 '>View Code</p>
         </Link>
-        <Link href={previewUrl} className='h-11 w-36 flex justify-center border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'>
+        <Link href={previewUrl} className='h-11 w-36 flex justify-center border-2 relative rounded-full border-[#0987e1] hover:border-white group/link'>
           <p className=' text-[#0987e1] cursor-pointer group-hover/link:text-white m-2 '>Preview Site</p>
         </Link>
         </div>

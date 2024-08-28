51d7e1f551c64355fdca7c98e8ea725333d57f39
HOWLADAR, ROMAN <131019051+roman0190@users.noreply.github.com>
2024-08-29 03:19:41 +0600

    little change

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import CustomCursor from "./components/CustomCursor";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <CustomCursor />
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }

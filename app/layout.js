4b10341c443070da4417b9cdbc3df0bc0ce6b0c9
HOWLADAR, ROMAN <131019051+roman0190@users.noreply.github.com>
2025-03-25 05:54:39 +0600

    Dhur

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import ThreeBackground from "./components/ThreeBackground";
-import CustomCursor from "./components/CustomCursor";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,23 +8,21 @@ export const metadata = {
   title: "Roman | Portfolio",
   description:
     "Roman is a full-stack developer with expertise in web development",
-  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
       <body
         className={`${inter.className} bg-white dark:bg-[#121212] text-black dark:text-white transition-colors duration-300`}
       >
-        <ThreeBackground />
-        <ThemeProvider>
-          <CustomCursor />
-          {children}
-        </ThemeProvider>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
